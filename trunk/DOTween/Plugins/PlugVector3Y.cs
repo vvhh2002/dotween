14ffaaed1cbbe3ecda86362c4b101ddad0eedea4
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-07-11 08:56:09 +0000

    Added PlugVector3Y/Z and global Plug shortcuts

--- trunk/DOTween/Plugins/PlugVector3Y.cs
+++ trunk/DOTween/Plugins/PlugVector3Y.cs
@@ -1,5 +1,5 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
-// Created: 2014/07/07 14:33
+// Created: 2014/07/11 10:38
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -30,21 +30,21 @@ namespace DG.Tween.Plugins
     // ||| PLUG ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    public struct PlugVector3X : IPlugSetter<Vector3, float, Vector3XPlugin, PlugVector3X.Options>
+    public struct PlugVector3Y : IPlugSetter<Vector3, float, Vector3YPlugin, PlugVector3Y.Options>
     {
         readonly float _endValue;
         readonly MemberGetter<Vector3> _getter;
         readonly MemberSetter<Vector3> _setter;
         readonly Options _options;
 
-        public PlugVector3X(MemberGetter<Vector3> getter, MemberSetter<Vector3> setter, float endValue)
+        public PlugVector3Y(MemberGetter<Vector3> getter, MemberSetter<Vector3> setter, float endValue)
         {
             _getter = getter;
             _setter = setter;
             _endValue = endValue;
             _options = new Options();
         }
-        public PlugVector3X(MemberGetter<Vector3> getter, MemberSetter<Vector3> setter, float endValue, Options options)
+        public PlugVector3Y(MemberGetter<Vector3> getter, MemberSetter<Vector3> setter, float endValue, Options options)
         {
             _getter = getter;
             _setter = setter;
@@ -76,28 +76,28 @@ namespace DG.Tween.Plugins
     // ||| PLUGIN ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    public class Vector3XPlugin : ABSTweenPlugin<Vector3, float, PlugVector3X.Options>
+    public class Vector3YPlugin : ABSTweenPlugin<Vector3, float, PlugVector3Y.Options>
     {
-        public override float ConvertT1toT2(PlugVector3X.Options options, Vector3 value)
+        public override float ConvertT1toT2(PlugVector3Y.Options options, Vector3 value)
         {
-            return value.x;
+            return value.y;
         }
 
-        public override float GetRelativeEndValue(PlugVector3X.Options options, float startValue, float changeValue)
+        public override float GetRelativeEndValue(PlugVector3Y.Options options, float startValue, float changeValue)
         {
             return startValue + changeValue;
         }
 
-        public override float GetChangeValue(PlugVector3X.Options options, float startValue, float endValue)
+        public override float GetChangeValue(PlugVector3Y.Options options, float startValue, float endValue)
         {
             return endValue - startValue;
         }
 
-        public override Vector3 Calculate(PlugVector3X.Options options, MemberGetter<Vector3> getter, float elapsed, float startValue, float changeValue, float duration, EaseFunction ease)
+        public override Vector3 Calculate(PlugVector3Y.Options options, MemberGetter<Vector3> getter, float elapsed, float startValue, float changeValue, float duration, EaseFunction ease)
         {
             Vector3 res = getter();
-            res.x = ease(elapsed, startValue, changeValue, duration, 0, 0);
-            if (options.snapping) res.x = Mathf.Round(res.x);
+            res.y = ease(elapsed, startValue, changeValue, duration, 0, 0);
+            if (options.snapping) res.y = Mathf.Round(res.y);
             return res;
         }
     }
