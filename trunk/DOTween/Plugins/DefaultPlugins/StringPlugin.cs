21b8c18f1026ce5522c2bd45ca1d426b14aed9d3
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-07-13 16:22:10 +0000

    StringPlugin now uses only uppercases letters for scrambling (visually better)

--- trunk/DOTween/Plugins/DefaultPlugins/StringPlugin.cs
+++ trunk/DOTween/Plugins/DefaultPlugins/StringPlugin.cs
@@ -86,8 +86,8 @@ namespace DG.Tweening.Plugins.DefaultPlugins
     {
         static readonly char[] _ScrambledChars = new[] {
             'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z',
-            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z',
-            '1','2','3','4','5','6','7','8','9','0'
+//            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z',
+//            '1','2','3','4','5','6','7','8','9','0'
         };
         static readonly int _ScrambledCharsLen;
         static int _lastRndSeed;
