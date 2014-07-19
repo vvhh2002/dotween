1ab5b30d7e135996d67f18ba9f92596ef9c4e34c
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-07-19 17:29:15 +0000

    Ease functions are now directly inside Ease class (except for Bounce). Old ease classes still kept for reference

--- DOTween/Core/Easing/Ease.cs
+++ DOTween/Core/Easing/Ease.cs
@@ -1,45 +1,122 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
 // Created: 2014/07/19 14:11
+
+using System;
+using UnityEngine;
+
 namespace DG.Tweening.Core.Easing
 {
     public static class Ease
     {
+        const float _PiOver2 = Mathf.PI * 0.5f;
+        const float _TwoPi = Mathf.PI * 2;
+
         public static float Apply(Tween t, float time, float startValue, float changeValue, float duration, float overshootOrAmplitude, float period)
         {
             switch (t.easeType) {
-                case EaseType.Linear: return Linear.EaseNone(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InSine: return Sine.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutSine: return Sine.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutSine: return Sine.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InQuad: return Quad.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutQuad: return Quad.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutQuad: return Quad.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InCubic: return Cubic.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutCubic: return Cubic.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutCubic: return Cubic.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InQuart: return Quart.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutQuart: return Quart.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutQuart: return Quart.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InQuint: return Quint.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutQuint: return Quint.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutQuint: return Quint.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InExpo: return Expo.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutExpo: return Expo.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutExpo: return Expo.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InCirc: return Circ.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutCirc: return Circ.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutCirc: return Circ.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InElastic: return Elastic.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutElastic: return Elastic.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutElastic: return Elastic.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InBack: return Back.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutBack: return Back.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutBack: return Back.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InBounce: return Bounce.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.OutBounce: return Bounce.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.InOutBounce: return Bounce.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                case EaseType.AnimationCurve: return t.easeCurveEval(time, startValue, changeValue, duration, overshootOrAmplitude, period);
-                default: return Quad.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
+            case EaseType.Linear:
+                return changeValue * time / duration + startValue;
+            case EaseType.InSine:
+                return -changeValue * (float)Math.Cos(time / duration * _PiOver2) + changeValue + startValue;
+            case EaseType.OutSine:
+                return changeValue*(float)Math.Sin(time/duration*_PiOver2) + startValue;
+            case EaseType.InOutSine:
+                return -changeValue * 0.5f * ((float)Math.Cos(Mathf.PI * time / duration) - 1) + startValue;
+            case EaseType.InQuad:
+                return changeValue*(time /= duration)*time + startValue;
+            case EaseType.OutQuad:
+                return -changeValue * (time /= duration) * (time - 2) + startValue;
+            case EaseType.InOutQuad:
+                if ((time /= duration*0.5f) < 1) return changeValue*0.5f*time*time + startValue;
+                return -changeValue*0.5f*((--time)*(time - 2) - 1) + startValue;
+            case EaseType.InCubic:
+                return changeValue * (time /= duration) * time * time + startValue;
+            case EaseType.OutCubic:
+                return changeValue * ((time = time / duration - 1) * time * time + 1) + startValue;
+            case EaseType.InOutCubic:
+                if ((time /= duration*0.5f) < 1) return changeValue*0.5f*time*time*time + startValue;
+                return changeValue*0.5f*((time -= 2)*time*time + 2) + startValue;
+            case EaseType.InQuart:
+                return changeValue * (time /= duration) * time * time * time + startValue;
+            case EaseType.OutQuart:
+                return -changeValue * ((time = time / duration - 1) * time * time * time - 1) + startValue;
+            case EaseType.InOutQuart:
+                if ((time /= duration*0.5f) < 1) return changeValue*0.5f*time*time*time*time + startValue;
+                return -changeValue*0.5f*((time -= 2)*time*time*time - 2) + startValue;
+            case EaseType.InQuint:
+                return changeValue*(time /= duration)*time*time*time*time + startValue;
+            case EaseType.OutQuint:
+                return changeValue*((time = time/duration - 1)*time*time*time*time + 1) + startValue;
+            case EaseType.InOutQuint:
+                if ((time /= duration*0.5f) < 1) return changeValue*0.5f*time*time*time*time*time + startValue;
+                return changeValue*0.5f*((time -= 2)*time*time*time*time + 2) + startValue;
+            case EaseType.InExpo:
+                if (time == 0) return startValue;
+                return changeValue*(float)Math.Pow(2, 10*(time/duration - 1)) + startValue - changeValue*0.001f;
+            case EaseType.OutExpo:
+                if (time == duration) return startValue + changeValue;
+                return changeValue*(-(float)Math.Pow(2, -10*time/duration) + 1) + startValue;
+            case EaseType.InOutExpo:
+                if (time == 0) return startValue;
+                if (time == duration) return startValue + changeValue;
+                if ((time /= duration*0.5f) < 1) return changeValue*0.5f*(float)Math.Pow(2, 10*(time - 1)) + startValue;
+                return changeValue*0.5f*(-(float)Math.Pow(2, -10*--time) + 2) + startValue;
+            case EaseType.InCirc:
+                return -changeValue*((float)Math.Sqrt(1 - (time /= duration)*time) - 1) + startValue;
+            case EaseType.OutCirc:
+                return changeValue * (float)Math.Sqrt(1 - (time = time / duration - 1) * time) + startValue;
+            case EaseType.InOutCirc:
+                if ((time /= duration*0.5f) < 1) return -changeValue*0.5f*((float)Math.Sqrt(1 - time*time) - 1) + startValue;
+                return changeValue*0.5f*((float)Math.Sqrt(1 - (time -= 2)*time) + 1) + startValue;
+            case EaseType.InElastic:
+                float s0;
+                if (time == 0) return startValue;
+                if ((time /= duration) == 1) return startValue + changeValue;
+                if (period == 0) period = duration*0.3f;
+                if (overshootOrAmplitude == 0 || (changeValue > 0 && overshootOrAmplitude < changeValue) || (changeValue < 0 && overshootOrAmplitude < -changeValue)) {
+                    overshootOrAmplitude = changeValue;
+                    s0 = period/4;
+                } else s0 = period / _TwoPi * (float)Math.Asin(changeValue / overshootOrAmplitude);
+                return -(overshootOrAmplitude * (float)Math.Pow(2, 10 * (time -= 1)) * (float)Math.Sin((time * duration - s0) * _TwoPi / period)) + startValue;
+            case EaseType.OutElastic:
+                float s1;
+                if (time == 0) return startValue;
+                if ((time /= duration) == 1) return startValue + changeValue;
+                if (period == 0) period = duration*0.3f;
+                if (overshootOrAmplitude == 0 || (changeValue > 0 && overshootOrAmplitude < changeValue) || (changeValue < 0 && overshootOrAmplitude < -changeValue)) {
+                    overshootOrAmplitude = changeValue;
+                    s1 = period/4;
+                } else s1 = period / _TwoPi * (float)Math.Asin(changeValue / overshootOrAmplitude);
+                return (overshootOrAmplitude * (float)Math.Pow(2, -10 * time) * (float)Math.Sin((time * duration - s1) * _TwoPi / period) + changeValue + startValue);
+            case EaseType.InOutElastic:
+                float s;
+                if (time == 0) return startValue;
+                if ((time /= duration*0.5f) == 2) return startValue + changeValue;
+                if (period == 0) period = duration*(0.3f*1.5f);
+                if (overshootOrAmplitude == 0 || (changeValue > 0 && overshootOrAmplitude < changeValue) || (changeValue < 0 && overshootOrAmplitude < -changeValue)) {
+                    overshootOrAmplitude = changeValue;
+                    s = period/4;
+                } else s = period / _TwoPi * (float)Math.Asin(changeValue / overshootOrAmplitude);
+                if (time < 1) return -0.5f * (overshootOrAmplitude * (float)Math.Pow(2, 10 * (time -= 1)) * (float)Math.Sin((time * duration - s) * _TwoPi / period)) + startValue;
+                return overshootOrAmplitude * (float)Math.Pow(2, -10 * (time -= 1)) * (float)Math.Sin((time * duration - s) * _TwoPi / period) * 0.5f + changeValue + startValue;
+            case EaseType.InBack:
+                return changeValue * (time /= duration) * time * ((overshootOrAmplitude + 1) * time - overshootOrAmplitude) + startValue;
+            case EaseType.OutBack:
+                return changeValue*((time = time/duration - 1)*time*((overshootOrAmplitude + 1)*time + overshootOrAmplitude) + 1) + startValue;
+            case EaseType.InOutBack:
+                if ((time /= duration * 0.5f) < 1) return changeValue * 0.5f * (time * time * (((overshootOrAmplitude *= (1.525f)) + 1) * time - overshootOrAmplitude)) + startValue;
+                return changeValue / 2 * ((time -= 2) * time * (((overshootOrAmplitude *= (1.525f)) + 1) * time + overshootOrAmplitude) + 2) + startValue;
+            case EaseType.InBounce:
+                return Bounce.EaseIn(time, startValue, changeValue, duration, overshootOrAmplitude, period);
+            case EaseType.OutBounce:
+                return Bounce.EaseOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
+            case EaseType.InOutBounce:
+                return Bounce.EaseInOut(time, startValue, changeValue, duration, overshootOrAmplitude, period);
+            case EaseType.AnimationCurve:
+                return t.easeCurveEval(time, startValue, changeValue, duration, overshootOrAmplitude, period);
+            default:
+                // OutQuad
+                return -changeValue * (time /= duration) * (time - 2) + startValue;
             }
         }
     }
