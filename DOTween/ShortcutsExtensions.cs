b8869ebd0dc89b5c11a43c0cebd09ecb7b1141ce
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-07-28 12:20:37 +0000

    Added extensions to directly Play/Pause/Kill/Goto a tween from a transform/material (myTransform.Play())

--- DOTween/ShortcutsExtensions.cs
+++ DOTween/ShortcutsExtensions.cs
@@ -11,17 +11,22 @@ namespace DG.Tweening
     /// </summary>
     public static class ShortcutsExtensions
     {
+        // ===================================================================================
+        // CREATION SHORTCUTS ----------------------------------------------------------------
+
         /////////////////////////////////////////////////////
         // Transform Shortcuts //////////////////////////////
 
-        /// <summary>Tweens a Transform's position to the given value</summary>
+        /// <summary>Tweens a Transform's position to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOMove(this Transform transform, Vector3 endValue, float duration)
         {
             return DOTween.To(() => transform.position, x => transform.position = x, endValue, duration).SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's X position to the given value</summary>
+        /// <summary>Tweens a Transform's X position to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOMoveX(this Transform transform, float endValue, float duration)
         {
@@ -30,7 +35,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Y position to the given value</summary>
+        /// <summary>Tweens a Transform's Y position to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOMoveY(this Transform transform, float endValue, float duration)
         {
@@ -39,7 +45,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Z position to the given value</summary>
+        /// <summary>Tweens a Transform's Z position to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOMoveZ(this Transform transform, float endValue, float duration)
         {
@@ -48,14 +55,16 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's localPosition to the given value</summary>
+        /// <summary>Tweens a Transform's localPosition to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOLocalMove(this Transform transform, Vector3 endValue, float duration)
         {
             return DOTween.To(() => transform.localPosition, x => transform.localPosition = x, endValue, duration).SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's X localPosition to the given value</summary>
+        /// <summary>Tweens a Transform's X localPosition to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOLocalMoveX(this Transform transform, float endValue, float duration)
         {
@@ -64,7 +73,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Y localPosition to the given value</summary>
+        /// <summary>Tweens a Transform's Y localPosition to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOLocalMoveY(this Transform transform, float endValue, float duration)
         {
@@ -73,7 +83,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Z localPosition to the given value</summary>
+        /// <summary>Tweens a Transform's Z localPosition to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOLocalMoveZ(this Transform transform, float endValue, float duration)
         {
@@ -82,28 +93,32 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's rotation to the given value</summary>
+        /// <summary>Tweens a Transform's rotation to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DORotate(this Transform transform, Vector3 endValue, float duration)
         {
             return DOTween.To(() => transform.rotation, x => transform.rotation = x, endValue, duration).SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's localRotation to the given value</summary>
+        /// <summary>Tweens a Transform's localRotation to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOLocalRotate(this Transform transform, Vector3 endValue, float duration)
         {
             return DOTween.To(() => transform.localRotation, x => transform.localRotation = x, endValue, duration).SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's localScale to the given value</summary>
+        /// <summary>Tweens a Transform's localScale to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOScale(this Transform transform, Vector3 endValue, float duration)
         {
             return DOTween.To(() => transform.localScale, x => transform.localScale = x, endValue, duration).SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's X localScale to the given value</summary>
+        /// <summary>Tweens a Transform's X localScale to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOScaleX(this Transform transform, float endValue, float duration)
         {
@@ -112,7 +127,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Y localScale to the given value</summary>
+        /// <summary>Tweens a Transform's Y localScale to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOScaleY(this Transform transform, float endValue, float duration)
         {
@@ -121,7 +137,8 @@ namespace DG.Tweening
                 .SetId(transform);
         }
 
-        /// <summary>Tweens a Transform's Z localScale to the given value</summary>
+        /// <summary>Tweens a Transform's Z localScale to the given value.
+        /// Also adds the transform as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOScaleZ(this Transform transform, float endValue, float duration)
         {
@@ -133,7 +150,8 @@ namespace DG.Tweening
         /////////////////////////////////////////////////////
         // Material Shortcuts ///////////////////////////////
 
-        /// <summary>Tweens a Material's color to the given value</summary>
+        /// <summary>Tweens a Material's color to the given value.
+        /// Also adds the material as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOColor(this Material material, Color endValue, float duration)
         {
@@ -141,12 +159,212 @@ namespace DG.Tweening
         }
 
         /// <summary>Tweens a Material's alpha color to the given value
-        /// (will have no effect unless your material supports transparency)</summary>
+        /// (will have no effect unless your material supports transparency).
+        /// Also adds the material as the tween Id so it can be used for filtered operations</summary>
         /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
         public static Tweener DOFade(this Material material, float endValue, float duration)
         {
             return DOTween.ToAlpha(() => material.color, x => material.color = x, endValue, duration)
                 .SetId(material);
+        }
+
+        // ===================================================================================
+        // OPERATION SHORTCUTS ---------------------------------------------------------------
+
+        /// <summary>
+        /// Kills all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens killed.
+        /// </summary>
+        public static int DOKill(this Transform transform)
+        {
+            return DOTween.Kill(transform);
+        }
+        /// <summary>
+        /// Kills all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens killed.
+        /// </summary>
+        public static int DOKill(this Material material)
+        {
+            return DOTween.Kill(material);
+        }
+
+        /// <summary>
+        /// Flips the direction (backwards if it was going forward or viceversa) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens flipped.
+        /// </summary>
+        public static int DOFlip(this Transform transform)
+        {
+            return DOTween.Flip(transform);
+        }
+        /// <summary>
+        /// Flips the direction (backwards if it was going forward or viceversa) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOFlip(this Material material)
+        {
+            return DOTween.Flip(material);
+        }
+
+        /// <summary>
+        /// Sends to the given position all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        /// <param name="to">Time position to reach
+        /// (if higher than the whole tween duration the tween will simply reach its end)</param>
+        /// <param name="andPlay">If TRUE will play the tween after reaching the given position, otherwise it will pause it</param>
+        public static int DOGoto(this Transform transform, float to, bool andPlay = false)
+        {
+            return DOTween.Goto(transform, to, andPlay);
+        }
+        /// <summary>
+        /// Sends to the given position all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        /// <param name="to">Time position to reach
+        /// (if higher than the whole tween duration the tween will simply reach its end)</param>
+        /// <param name="andPlay">If TRUE will play the tween after reaching the given position, otherwise it will pause it</param>
+        public static int DOGoto(this Material material, float to, bool andPlay = false)
+        {
+            return DOTween.Goto(material, to, andPlay);
+        }
+
+        /// <summary>
+        /// Pauses all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens paused.
+        /// </summary>
+        public static int DOPause(this Transform transform)
+        {
+            return DOTween.Pause(transform);
+        }
+        /// <summary>
+        /// Pauses all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens paused.
+        /// </summary>
+        public static int DOPause(this Material material)
+        {
+            return DOTween.Pause(material);
+        }
+
+        /// <summary>
+        /// Plays all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlay(this Transform transform)
+        {
+            return DOTween.Play(transform);
+        }
+        /// <summary>
+        /// Plays all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlay(this Material material)
+        {
+            return DOTween.Play(material);
+        }
+
+        /// <summary>
+        /// Plays backwards all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayBackwards(this Transform transform)
+        {
+            return DOTween.PlayBackwards(transform);
+        }
+        /// <summary>
+        /// Plays backwards all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayBackwards(this Material material)
+        {
+            return DOTween.PlayBackwards(material);
+        }
+
+        /// <summary>
+        /// Plays forward all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayForward(this Transform transform)
+        {
+            return DOTween.PlayForward(transform);
+        }
+        /// <summary>
+        /// Plays forward all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayForward(this Material material)
+        {
+            return DOTween.PlayForward(material);
+        }
+
+        /// <summary>
+        /// Restarts all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens restarted.
+        /// </summary>
+        public static int DORestart(this Transform transform)
+        {
+            return DOTween.Restart(transform);
+        }
+        /// <summary>
+        /// Restarts all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens restarted.
+        /// </summary>
+        public static int DORestart(this Material material)
+        {
+            return DOTween.Restart(material);
+        }
+
+        /// <summary>
+        /// Rewinds all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens rewinded.
+        /// </summary>
+        public static int DORewind(this Transform transform)
+        {
+            return DOTween.Rewind(transform);
+        }
+        /// <summary>
+        /// Rewinds all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens rewinded.
+        /// </summary>
+        public static int DORewind(this Material material)
+        {
+            return DOTween.Rewind(material);
+        }
+
+        /// <summary>
+        /// Toggles the paused state (plays if it was paused, pauses if it was playing) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        public static int DOTogglePause(this Transform transform)
+        {
+            return DOTween.TogglePause(transform);
+        }
+        /// <summary>
+        /// Toggles the paused state (plays if it was paused, pauses if it was playing) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        public static int DOTogglePause(this Material material)
+        {
+            return DOTween.TogglePause(material);
         }
     }
 }
