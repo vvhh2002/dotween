faacc40d4c90ad133e12c07c2f7e3ca9459c3d55
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-01 09:42:50 +0000

    Implemented TweenParms correctly

---
+++ trunk/UnityTests.Unity4/Assets/_Tests/TweenParmsBrain.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class TweenParmsBrain : BrainBase
+{
+	public Transform[] ts;
+
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds(0.5f);
+
+		TweenParms tp = new TweenParms()
+			.SetRelative()
+			// .SetSpeedBased()
+			.SetEase(Ease.OutQuint)
+			.SetLoops(-1, LoopType.Yoyo);
+
+		foreach (Transform t in ts) {
+			t.DOMoveY(2, 1).SetAs(tp);
+		}
+	}
+}
