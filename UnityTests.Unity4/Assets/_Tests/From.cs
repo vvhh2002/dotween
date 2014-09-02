c67081b1ae18cdf716d6d3550e30ad28239d55da
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-02 08:50:04 +0000

    - Quaternion tweens now follow shortest 360° route unless chosen differently - Quaternions/DORotate: added choice to go beyond 360° or not

---
+++ UnityTests.Unity4/Assets/_Tests/From.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class From : BrainBase
+{
+	public GameObject prefab;
+
+	Transform t;
+
+	void LateUpdate()
+	{
+		if (t != null) Debug.Log(">>> " + t.position);
+	}
+
+	void OnGUI()
+	{
+		DGUtils.BeginGUI();
+
+		if (GUILayout.Button("New FROM")) FromTween();
+
+		DGUtils.EndGUI();
+	}
+
+	void FromTween()
+	{
+		t = ((GameObject)Instantiate(prefab)).transform;
+		t.DOMoveFrom(new Vector3(0, 2, 0), 1)
+			.OnKill(() => Destroy(t.gameObject));
+
+		Debug.Log(t.position);
+	}
+}
