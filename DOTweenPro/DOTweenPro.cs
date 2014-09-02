c2bc07d15eb8c07b0de45bc10e2e821c2df0801e
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-02 17:52:16 +0000

    DOTweenPro version now shown in DOTween Inspector (if DOTweenPro is present)

---
+++ DOTweenPro/DOTweenPro.cs
@@ -0,0 +1,13 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2014/09/02 19:24
+// 
+// License Copyright (c) Daniele Giardini.
+// This work is subject to the terms at http://dotween.demigiant.com/license.php
+
+namespace DG.Tweening
+{
+    class DOTweenPro
+    {
+        public static readonly string Version = "0.5.000";
+    }
+}
