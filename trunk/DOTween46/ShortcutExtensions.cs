68f9d77516023879ffb18112fd5bc6cdbad3fe5e
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-29 10:09:05 +0000

    - Added DOTween46 DLL - Added UI Image shortcuts

---
+++ trunk/DOTween46/ShortcutExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2014/09/29 11:53
+// 
+// License Copyright (c) Daniele Giardini.
+// This work is subject to the terms at http://dotween.demigiant.com/license.php
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DG.Tweening
+{
+    /// <summary>
+    /// Methods that extend known Unity objects and allow to directly create and control tweens from their instances.
+    /// These, as all DOTween46 methods, require Unity 4.6 or later.
+    /// </summary>
+    public static class ShortcutExtensions
+    {
+        #region Unity UI > Image
+
+        /// <summary>Tweens a SpriteRenderer's color to the given value.
+        /// Also stores the spriteRenderer as the tween's target so it can be used for filtered operations</summary>
+        /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
+        public static Tweener DOColor(this Image target, Color endValue, float duration)
+        {
+            return DOTween.To(() => target.color, x => target.color = x, endValue, duration).SetTarget(target);
+        }
+
+        /// <summary>Tweens a Material's alpha color to the given value.
+        /// Also stores the spriteRenderer as the tween's target so it can be used for filtered operations</summary>
+        /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
+        public static Tweener DOFade(this Image target, float endValue, float duration)
+        {
+            return DOTween.ToAlpha(() => target.color, x => target.color = x, endValue, duration)
+                .SetTarget(target);
+        }
+
+        #endregion
+    }
+}
