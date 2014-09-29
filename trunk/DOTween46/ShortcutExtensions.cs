a3413254105708aff286a220abf17fb4587c1791
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-09-29 14:20:45 +0000

    - Added UI CanvasGroup shortcuts

--- trunk/DOTween46/ShortcutExtensions.cs
+++ trunk/DOTween46/ShortcutExtensions.cs
@@ -15,7 +15,22 @@ namespace DG.Tweening
     /// </summary>
     public static class ShortcutExtensions
     {
-        #region Unity UI > Image
+        #region Unity UI
+
+        #region CanvasGroup
+
+        /// <summary>Tweens a CanvasGroup's alpha color to the given value.
+        /// Also stores the canvasGroup as the tween's target so it can be used for filtered operations</summary>
+        /// <param name="endValue">The end value to reach</param><param name="duration">The duration of the tween</param>
+        public static Tweener DOFade(this CanvasGroup target, float endValue, float duration)
+        {
+            return DOTween.To(() => target.alpha, x => target.alpha = x, endValue, duration)
+                .SetTarget(target);
+        }
+
+        #endregion
+
+        #region Image
 
         /// <summary>Tweens an Image's color to the given value.
         /// Also stores the image as the tween's target so it can be used for filtered operations</summary>
@@ -33,6 +48,8 @@ namespace DG.Tweening
             return DOTween.ToAlpha(() => target.color, x => target.color = x, endValue, duration)
                 .SetTarget(target);
         }
+
+        #endregion
 
         #endregion
     }
