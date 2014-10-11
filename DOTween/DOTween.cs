cb50160b24e90cff6cdbf6f0e81757e364324ffa
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-10-11 17:30:39 +0000

    Added material.DOFloat/DOVector shortcuts

--- DOTween/DOTween.cs
+++ DOTween/DOTween.cs
@@ -21,7 +21,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "0.9.180";
+        public static readonly string Version = "0.9.190";
 
         ///////////////////////////////////////////////
         // Options ////////////////////////////////////
