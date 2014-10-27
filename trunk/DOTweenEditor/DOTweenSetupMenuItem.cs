d318003745a57cd37cd02311d8e9e5ac7fbf8f27
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-10-27 17:18:23 +0000

    Implemented additional Pro shortcuts import/setup in DOTween Setup

--- trunk/DOTweenEditor/DOTweenSetupMenuItem.cs
+++ trunk/DOTweenEditor/DOTweenSetupMenuItem.cs
@@ -18,21 +18,24 @@ namespace DG.DOTweenEditor
         static void StartSetup() { Setup(); }
 
         const string _Title = "DOTween Setup";
+        static bool _proExists;
 
-        static string _addedLibraries;
-		
         static void Setup()
         {
-            _addedLibraries = "";
-
             string codeBase = Assembly.GetExecutingAssembly().CodeBase;
             UriBuilder uri = new UriBuilder(codeBase);
             string addonsDir = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
             string pathSeparator = addonsDir.IndexOf("/") != -1 ? "/" : "\\";
             addonsDir = addonsDir.Substring(0, addonsDir.LastIndexOf(pathSeparator) + 1);
 
-            if (Directory.GetFiles(addonsDir, "*.addon").Length > 0) {
-                string msg = "Based on your Unity version (" + Application.unityVersion + "), DOTween will now import additional DOTween elements for more recent Unity features, if available.";
+            string proAddonsDir = addonsDir.Substring(0, addonsDir.LastIndexOf(pathSeparator));
+            proAddonsDir = proAddonsDir.Substring(0, proAddonsDir.LastIndexOf(pathSeparator) + 1) + "DOTweenPro" + pathSeparator;
+            _proExists = Directory.Exists(proAddonsDir);
+
+            string projectPath = Application.dataPath + pathSeparator;
+
+            if (Directory.GetFiles(addonsDir, "*.addon").Length > 0 || _proExists && Directory.GetFiles(proAddonsDir, "*.addon").Length > 0) {
+                string msg = "Based on your Unity version (" + Application.unityVersion + ") and eventual plugins, DOTween will now import additional tween elements, if available.";
                 if (!EditorUtility.DisplayDialog(_Title, msg, "Ok", "Cancel")) return;
             } else {
                 string msg = "This project has already been setup for your version of DOTween.\nUpdate to a new DOTween version before running the setup again.";
@@ -40,32 +43,42 @@ namespace DG.DOTweenEditor
                 return;
             }
 
-            EditorUtility.DisplayProgressBar(_Title, "Importing additional DOTween elements based on your Unity version...", 0.25f);
+            EditorUtility.DisplayProgressBar(_Title, "Importing additional DOTween elements based on your Unity version and eventual plugins...", 0.25f);
 
             int totImported = 0;
+            // Unity version-based files
             string[] vs = Application.unityVersion.Split("."[0]);
             int majorVersion = Convert.ToInt32(vs[0]);
             int minorVersion = Convert.ToInt32(vs[1]);
             if (majorVersion < 4) {
-                SetupComplete(addonsDir, totImported);
+                SetupComplete(addonsDir, proAddonsDir, totImported);
                 return;
             }
             if (majorVersion == 4) {
                 if (minorVersion < 3) {
-                    SetupComplete(addonsDir, totImported);
+                    SetupComplete(addonsDir, proAddonsDir, totImported);
                     return;
                 }
-                totImported += ImportDll(43, addonsDir);
-                if (minorVersion >= 6) totImported += ImportDll(46, addonsDir);
+                totImported += ImportAddons("43", addonsDir);
+                if (minorVersion >= 6) totImported += ImportAddons("46", addonsDir);
             } else {
                 // 5.x
-                totImported += ImportDll(43, addonsDir);
-                totImported += ImportDll(46, addonsDir);
+                totImported += ImportAddons("43", addonsDir);
+                totImported += ImportAddons("46", addonsDir);
             }
-            SetupComplete(addonsDir, totImported);
+            // Additional plugin files
+            // Pro plugins
+            if (_proExists) {
+                if (Directory.Exists(projectPath + "TK2DROOT")) {
+                    // PRO > 2DToolkit shortcuts
+                    totImported += ImportAddons("Tk2d", proAddonsDir);
+                }
+            }
+
+            SetupComplete(addonsDir, proAddonsDir, totImported);
         }
 
-        static void SetupComplete(string addonsDir, int totImported)
+        static void SetupComplete(string addonsDir, string proAddonsDir, int totImported)
         {
 
             // Delete all remaining addon files
@@ -74,29 +87,43 @@ namespace DG.DOTweenEditor
                 EditorUtility.DisplayProgressBar(_Title, "Removing " + leftoverAddonFiles.Length + " unused additional files...", 0.5f);
                 foreach (string leftoverAddonFile in leftoverAddonFiles) File.Delete(leftoverAddonFile);
             }
+            if (_proExists) {
+                leftoverAddonFiles = Directory.GetFiles(proAddonsDir, "*.addon");
+                if (leftoverAddonFiles.Length > 0) {
+                    EditorUtility.DisplayProgressBar(_Title, "Removing " + leftoverAddonFiles.Length + " unused additional files...", 0.5f);
+                    foreach (string leftoverAddonFile in leftoverAddonFiles) File.Delete(leftoverAddonFile);
+                }
+            }
             // Delete all remaining addon meta files
             leftoverAddonFiles = Directory.GetFiles(addonsDir, "*.addon.meta");
             if (leftoverAddonFiles.Length > 0) {
                 EditorUtility.DisplayProgressBar(_Title, "Removing " + leftoverAddonFiles.Length + " unused additional meta files...", 0.75f);
                 foreach (string leftoverAddonFile in leftoverAddonFiles) File.Delete(leftoverAddonFile);
             }
+            if (_proExists) {
+                leftoverAddonFiles = Directory.GetFiles(proAddonsDir, "*.addon.meta");
+                if (leftoverAddonFiles.Length > 0) {
+                    EditorUtility.DisplayProgressBar(_Title, "Removing " + leftoverAddonFiles.Length + " unused additional meta files...", 0.75f);
+                    foreach (string leftoverAddonFile in leftoverAddonFiles) File.Delete(leftoverAddonFile);
+                }
+            }
 
             EditorUtility.DisplayProgressBar(_Title, "Refreshing...", 0.9f);
             AssetDatabase.Refresh();
 
             EditorUtility.ClearProgressBar();
-            EditorUtility.DisplayDialog(_Title, "DOTween setup is now complete." + (totImported == 0 ? "" : "\n" + (totImported / 3) + " additional libraries were imported or updated."), "Ok");
-            _addedLibraries = null;
+            EditorUtility.DisplayDialog(_Title, "DOTween setup is now complete." + (totImported == 0 ? "" : "\n" + totImported + " additional libraries were imported or updated."), "Ok");
         }
 
         // Removes relative .addon extension thus activating files
-        static int ImportDll(int version, string addonsDir)
+        static int ImportAddons(string version, string addonsDir)
         {
-            int totImported = 0;
+            bool imported = false;
             string[] filenames = new[] {
                 "DOTween" + version + ".dll",
                 "DOTween" + version + ".xml",
-                "DOTween" + version + ".dll.mdb"
+                "DOTween" + version + ".dll.mdb",
+                "DOTween" + version + ".cs"
             };
 
             foreach (string filename in filenames) {
@@ -107,11 +134,11 @@ namespace DG.DOTweenEditor
                     if (File.Exists(finalFilepath)) File.Delete(finalFilepath);
                     // Rename addon file to final
                     File.Move(addonFilepath, finalFilepath);
-                    totImported++;
+                    imported = true;
                 }
             }
 
-            return totImported;
+            return imported ? 1 : 0;
         }
     }
 }
