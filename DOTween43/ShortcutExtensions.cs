cecb89917f48d4696af28205d9c3cf780d352331
daniele.giardini@gmail.com <daniele.giardini@gmail.com@7c4ab8a0-76a6-45ed-d0f5-36f15d7fc04d>
2014-12-11 20:01:23 +0000

    Added Unity34 operation shortcuts

--- DOTween43/ShortcutExtensions.cs
+++ DOTween43/ShortcutExtensions.cs
@@ -80,5 +80,208 @@ namespace DG.Tweening
         }
 
         #endregion
+
+        // ===================================================================================
+        // OPERATION SHORTCUTS ---------------------------------------------------------------
+
+        #region Operation Shortcuts
+
+        /// <summary>
+        /// Kills all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens killed.
+        /// </summary>
+        public static int DOKill(this SpriteRenderer target)
+        {
+            return DOTween.Kill(target);
+        }
+        /// <summary>
+        /// Kills all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens killed.
+        /// </summary>
+        public static int DOKill(this Rigidbody2D target)
+        {
+            return DOTween.Kill(target);
+        }
+
+        /// <summary>
+        /// Flips the direction (backwards if it was going forward or viceversa) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens flipped.
+        /// </summary>
+        public static int DOFlip(this SpriteRenderer target)
+        {
+            return DOTween.Flip(target);
+        }
+        /// <summary>
+        /// Flips the direction (backwards if it was going forward or viceversa) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens flipped.
+        /// </summary>
+        public static int DOFlip(this Rigidbody2D target)
+        {
+            return DOTween.Flip(target);
+        }
+
+        /// <summary>
+        /// Sends to the given position all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        /// <param name="to">Time position to reach
+        /// (if higher than the whole tween duration the tween will simply reach its end)</param>
+        /// <param name="andPlay">If TRUE will play the tween after reaching the given position, otherwise it will pause it</param>
+        public static int DOGoto(this SpriteRenderer target, float to, bool andPlay = false)
+        {
+            return DOTween.Goto(target, to, andPlay);
+        }
+        /// <summary>
+        /// Sends to the given position all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        /// <param name="to">Time position to reach
+        /// (if higher than the whole tween duration the tween will simply reach its end)</param>
+        /// <param name="andPlay">If TRUE will play the tween after reaching the given position, otherwise it will pause it</param>
+        public static int DOGoto(this Rigidbody2D target, float to, bool andPlay = false)
+        {
+            return DOTween.Goto(target, to, andPlay);
+        }
+
+        /// <summary>
+        /// Pauses all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens paused.
+        /// </summary>
+        public static int DOPause(this SpriteRenderer target)
+        {
+            return DOTween.Pause(target);
+        }
+        /// <summary>
+        /// Pauses all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens paused.
+        /// </summary>
+        public static int DOPause(this Rigidbody2D target)
+        {
+            return DOTween.Pause(target);
+        }
+
+        /// <summary>
+        /// Plays all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlay(this SpriteRenderer target)
+        {
+            return DOTween.Play(target);
+        }
+        /// <summary>
+        /// Plays all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlay(this Rigidbody2D target)
+        {
+            return DOTween.Play(target);
+        }
+
+        /// <summary>
+        /// Plays backwards all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayBackwards(this SpriteRenderer target)
+        {
+            return DOTween.PlayBackwards(target);
+        }
+        /// <summary>
+        /// Plays backwards all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayBackwards(this Rigidbody2D target)
+        {
+            return DOTween.PlayBackwards(target);
+        }
+
+        /// <summary>
+        /// Plays forward all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayForward(this SpriteRenderer target)
+        {
+            return DOTween.PlayForward(target);
+        }
+        /// <summary>
+        /// Plays forward all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens played.
+        /// </summary>
+        public static int DOPlayForward(this Rigidbody2D target)
+        {
+            return DOTween.PlayForward(target);
+        }
+
+        /// <summary>
+        /// Restarts all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens restarted.
+        /// </summary>
+        public static int DORestart(this SpriteRenderer target)
+        {
+            return DOTween.Restart(target);
+        }
+        /// <summary>
+        /// Restarts all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens restarted.
+        /// </summary>
+        public static int DORestart(this Rigidbody2D target)
+        {
+            return DOTween.Restart(target);
+        }
+
+        /// <summary>
+        /// Rewinds all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens rewinded.
+        /// </summary>
+        public static int DORewind(this SpriteRenderer target)
+        {
+            return DOTween.Rewind(target);
+        }
+        /// <summary>
+        /// Rewinds all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens rewinded.
+        /// </summary>
+        public static int DORewind(this Rigidbody2D target)
+        {
+            return DOTween.Rewind(target);
+        }
+
+        /// <summary>
+        /// Toggles the paused state (plays if it was paused, pauses if it was playing) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        public static int DOTogglePause(this SpriteRenderer target)
+        {
+            return DOTween.TogglePause(target);
+        }
+        /// <summary>
+        /// Toggles the paused state (plays if it was paused, pauses if it was playing) of all tweens that have this target as a reference
+        /// (meaning tweens that were started from this target, or that had this target added as an Id)
+        /// and returns the total number of tweens involved.
+        /// </summary>
+        public static int DOTogglePause(this Rigidbody2D target)
+        {
+            return DOTween.TogglePause(target);
+        }
+
+        #endregion
     }
 }
--- UnityTests.Unity4/Assets/_Tests/Rigidbodies.cs
+++ UnityTests.Unity4/Assets/_Tests/Rigidbodies.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rigidbodies : BrainBase
 {
 	public Transform[] targets;
+	public Rigidbody2D r2D;
 
 	IEnumerator Start()
 	{
@@ -14,5 +15,15 @@ public class Rigidbodies : BrainBase
 			.Append(targets[0].rigidbody.DORotate(new Vector3(0, 0, 90), 1, RotateMode.WorldAxisAdd))
 			.Append(targets[0].rigidbody.DORotate(new Vector3(90, 0, 0), 1, RotateMode.LocalAxisAdd))
 			.Append(targets[0].rigidbody.DORotate(new Vector3(0, 810, 0), 1, RotateMode.LocalAxisAdd));
+	}
+
+	void OnGUI()
+	{
+		DGUtils.BeginGUI();
+
+		if (GUILayout.Button("Rotate 2D 360")) r2D.DORotate(360, 1);
+		if (GUILayout.Button("Rotate 2D 360 Relative")) r2D.DORotate(360, 1).SetRelative();
+
+		DGUtils.EndGUI();
 	}
 }
